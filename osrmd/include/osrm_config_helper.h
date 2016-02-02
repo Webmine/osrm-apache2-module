8f06a670460af8c56acff4cbc3449b100d24a19e
heringei.daniel@webmine.hu <heringei.daniel@webmine.hu>
2016-02-02 11:11:35 +0100

    osrm config helper

---
+++ osrmd/include/osrm_config_helper.h
@@ -0,0 +1,32 @@
+#ifndef OSRM_CONFIG_HELPER_H_INCLUDED
+#define OSRM_CONFIG_HELPER_H_INCLUDED
+
+#include "logger/log.h"
+#include <unordered_map>
+#include "boost/filesystem/path.hpp"
+#include "boost/filesystem/operations.hpp"
+
+inline std::unordered_map<std::string,boost::filesystem::path> populate_server_paths(std::string base_path)
+{
+    std::unordered_map<std::string,boost::filesystem::path> server_paths;
+
+    server_paths["hsgrdata"] = base_path + ".hsgr";
+    server_paths["nodesdata"] = base_path + ".nodes";
+    server_paths["coredata"] = base_path + ".core";
+    server_paths["edgesdata"] = base_path + ".edges";
+    server_paths["geometries"] = base_path + ".geometry";
+    server_paths["ramindex"] = base_path + ".ramIndex";
+    server_paths["fileindex"] = base_path + ".fileIndex";
+    server_paths["namesdata"] = base_path + ".names";
+    server_paths["timestamp"] = base_path + ".timestamp";
+
+    for(auto it = server_paths.begin(); it != server_paths.end(); ++it)
+    {
+        if(!boost::filesystem::is_regular_file(it->second))
+        {
+            LOG_ERROR("File not exists %s", it->second.string().c_str());
+        }
+    }
+}
+
+#endif // OSRM_CONFIG_HELPER_H_INCLUDED
--- osrmd/src/ClientThread.cpp
+++ osrmd/src/ClientThread.cpp
@@ -1,4 +1,5 @@
 #include "ClientThread.h"
+#include "osrm_config_helper.h"
 #include "osrm/engine_config.hpp"
 #include "osrm/osrm.hpp"
 #include "osrm/route_parameters.hpp"
@@ -15,6 +16,10 @@ ClientThread::ClientThread(Server* server, int number)
 {
     this->server = server;
     this->threadNum = number;
+    EngineConfig config = EngineConfig();
+    config.server_paths = populate_server_paths("");
+    config.use_shared_memory = false;
+    this->osrmEngine = new OSRM(config);
 }
 
 ClientThread::~ClientThread()
@@ -51,9 +56,9 @@ void ClientThread::InternalThreadEntry()
 
         osrm::json::Object json_result;
         const int result_code = osrmEngine->RunQuery(route_parameters, json_result);
-        auto asd = json_result.values;
+        //auto asd = json_result.values;
 
-        std::cout << typeid(asd).name();
+        //std::cout << typeid(asd).name();
 
         //JSON j_umap();
 
